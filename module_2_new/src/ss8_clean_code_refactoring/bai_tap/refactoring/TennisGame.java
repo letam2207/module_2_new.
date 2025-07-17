89d136f5bd73fb6f8dcd0e2b8bd4b04fcaf6ef09
letam2207 <letam.x2004@gamil.com>
2025-07-17 08:06:41 +0700

    up bt

--- module_2_new/src/ss8_clean_code_refactoring/bai_tap/refactoring/TennisGame.java
+++ module_2_new/src/ss8_clean_code_refactoring/bai_tap/refactoring/TennisGame.java
@@ -1,68 +1,72 @@
 package ss8_clean_code_refactoring.bai_tap.refactoring;
 
 public class TennisGame {
-        private static final int ADVANTAGE_THRESHOLD = 4;
-        private static final int WIN_DIFFERENCE = 2;
 
-        public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
-            if (player1Score == player2Score) {
-                return getEqualScore(player1Score);
-            }
+    private static final int SCORE_LOVE = 0;
+    private static final int SCORE_FIFTEEN = 1;
+    private static final int SCORE_THIRTY = 2;
+    private static final int SCORE_FORTY = 3;
 
-            if (player1Score >= ADVANTAGE_THRESHOLD || player2Score >= ADVANTAGE_THRESHOLD) {
-                return getAdvantageOrWinScore(player1Score, player2Score);
-            }
+    private static final int ADVANTAGE_THRESHOLD = 4;
+    private static final int WIN_DIFFERENCE = 2;
 
-            return getNormalScore(player1Score, player2Score);
+    public static String getScore(String player1Name, String player2Name, int player1Score, int player2Score) {
+        if (player1Score == player2Score) {
+            return getEqualScore(player1Score);
         }
 
-        private static String getEqualScore(int score) {
-            switch (score) {
-                case 0:
-                    return "Love-All";
-                case 1:
-                    return "Fifteen-All";
-                case 2:
-                    return "Thirty-All";
-                case 3:
-                    return "Forty-All";
-                default:
-                    return "Deuce";
-            }
+        if (player1Score >= ADVANTAGE_THRESHOLD || player2Score >= ADVANTAGE_THRESHOLD) {
+            return getAdvantageOrWinScore(player1Score, player2Score);
         }
 
-        private static String getAdvantageOrWinScore(int player1Score, int player2Score) {
-            int scoreDifference = player1Score - player2Score;
-
-            if (scoreDifference == 1) {
-                return "Advantage player1";
-            } else if (scoreDifference == -1) {
-                return "Advantage player2";
-            } else if (scoreDifference >= WIN_DIFFERENCE) {
-                return "Win for player1";
-            } else {
-                return "Win for player2";
-            }
-        }
-
-        private static String getNormalScore(int player1Score, int player2Score) {
-            return getScoreString(player1Score) + "-" + getScoreString(player2Score);
-        }
-
-        private static String getScoreString(int score) {
-            switch (score) {
-                case 0:
-                    return "Love";
-                case 1:
-                    return "Fifteen";
-                case 2:
-                    return "Thirty";
-                case 3:
-                    return "Forty";
-                default:
-                    throw new IllegalArgumentException("Invalid score: " + score);
-            }
-        }
-
+        return getNormalScore(player1Score, player2Score);
     }
 
+    private static String getEqualScore(int score) {
+        switch (score) {
+            case SCORE_LOVE:
+                return "Love-All";
+            case SCORE_FIFTEEN:
+                return "Fifteen-All";
+            case SCORE_THIRTY:
+                return "Thirty-All";
+            case SCORE_FORTY:
+                return "Forty-All";
+            default:
+                return "Deuce";
+        }
+    }
+
+    private static String getAdvantageOrWinScore(int player1Score, int player2Score) {
+        int scoreDifference = player1Score - player2Score;
+
+        if (scoreDifference == 1) {
+            return "Advantage player1";
+        } else if (scoreDifference == -1) {
+            return "Advantage player2";
+        } else if (scoreDifference >= WIN_DIFFERENCE) {
+            return "Win for player1";
+        } else {
+            return "Win for player2";
+        }
+    }
+
+    private static String getNormalScore(int player1Score, int player2Score) {
+        return getScoreString(player1Score) + "-" + getScoreString(player2Score);
+    }
+
+    private static String getScoreString(int score) {
+        switch (score) {
+            case SCORE_LOVE:
+                return "Love";
+            case SCORE_FIFTEEN:
+                return "Fifteen";
+            case SCORE_THIRTY:
+                return "Thirty";
+            case SCORE_FORTY:
+                return "Forty";
+            default:
+                throw new IllegalArgumentException("Invalid score: " + score);
+        }
+    }
+}
--- module_2_new/src/ss8_clean_code_refactoring/bai_tap/refactoring/TennisGameTest.java
+++ module_2_new/src/ss8_clean_code_refactoring/bai_tap/refactoring/TennisGameTest.java
@@ -1,5 +1,7 @@
 package ss8_clean_code_refactoring.bai_tap.refactoring;
 
+import java.util.ArrayList;
+
 public class TennisGameTest {
 
 }
